7509994d9487659f8f03a1fb0aa064ca0b911291
Mcrymbo <croco2023@gmail.com>
2024-05-14 18:02:04 +0300

    app.use(express.json)

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,38 +1,37 @@
-import dbClient from '../utils/db';
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 class UsersController {
-    static async postNew(req, res) {
-        const { email, password } = req.body;
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-        // check for email and password
-        if (!email) return res.status(400).send({error: 'missing email'});
-        if (!password) return res.status(400).send({ error: 'Missing password'});
+    // check for email and password
+    if (!email) return res.status(400).send({ error: 'missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
 
-        // check if email exists in DB
-        const emailExist = await dbClient.users.findOne({email});
-        if (emailExist) return res.status(400).send({error: 'Already exist'});
+    // check if email exists in DB
+    const emailExist = await dbClient.users.findOne({ email });
+    if (emailExist) return res.status(400).send({ error: 'Already exist' });
 
-        // inserting a new user
-        const hasPassword = sha1(password);
-        let result;
+    // inserting a new user
+    const hasPassword = sha1(password);
+    let result;
 
-        try {
-            result = await dbClient.users.insertOne({
-                email, password: hasPassword,
-            });
-        } catch (err) {
-            return res.status(500).send({ error: 'Error creating user'});
-        }
-
-        const user = {
-            id: result.insertedId,
-            email,
-        };
-
-        return res.status(200).send(user);
+    try {
+      result = await dbClient.users.insertOne({
+        email, password: hasPassword,
+      });
+    } catch (err) {
+      return res.status(500).send({ error: 'Error creating user' });
     }
 
+    const user = {
+      id: result.insertedId,
+      email,
+    };
+
+    return res.status(200).send(user);
+  }
 }
 
 module.exports = UsersController;
--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
+
 app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
